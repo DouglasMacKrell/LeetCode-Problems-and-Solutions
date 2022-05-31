af80f941c9dc47c12eca503a0e7f25058ef620fd
Douglas MacKrell <52139563+DouglasMacKrell@users.noreply.github.com>
2022-05-31 18:06:40 -0400

    Time: 1 ms (39.77%), Space: 5.9 MB (83.64%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+    int num1 = 0;
+    int num2 = 1;
+    int sum;
+    for (int i = 0; i < n; ++i) 
+    {
+        sum = num1 + num2;
+        num1 = num2;
+        num2 = sum;
+    }
+    return num2;
+    }
+};
