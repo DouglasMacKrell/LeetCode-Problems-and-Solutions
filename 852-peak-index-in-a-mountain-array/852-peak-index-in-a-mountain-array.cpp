4c1781025fa5c7004f9e9b7ac13265485676a092
Douglas MacKrell <52139563+DouglasMacKrell@users.noreply.github.com>
2022-06-07 14:37:17 -0400

    Time: 27 ms (5.76%), Space: 11.6 MB (49.51%) - LeetHub

---
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int middle;
+        if (arr.size() % 2 != 0) {
+            middle = (arr.size() / 2) + 1;
+        } else {
+            middle = (arr.size() / 2);
+        }
+        
+        int left = 0;
+        int right = arr.size() - 1;
+        cout << middle;
+        while (left <= right) {
+            if (arr[middle - 1] < arr[middle] && arr[middle] > arr[middle + 1]) {
+                return middle;
+            } else {
+                if (arr[middle - 1] >= arr[middle] && arr[middle] >= arr[middle + 1]) {
+                    right = middle - 1;
+                } else {
+                    left = middle + 1;
+                }
+                if ((right - left) % 2 != 0) {
+                    middle = ((left + right) / 2) + 1;
+                } else {
+                    middle = (left + right) / 2;
+                }
+                
+                cout << middle;
+            }
+        }
+        return -1;
+    }
+};
