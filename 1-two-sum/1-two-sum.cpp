ce24f8b755e9478f95e1289860d85cff2565a30b
Douglas MacKrell <52139563+DouglasMacKrell@users.noreply.github.com>
2022-06-15 09:47:16 -0400

    Time: 56 ms (38.77%), Space: 13.2 MB (5.76%) - LeetHub

---
+++ 1-two-sum/1-two-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> result;
+        map<int, int> checkSum;
+        
+        for(int i = 0; i < nums.size() - 1; ++i){
+            int diff = target - nums[i];
+            checkSum[diff] = i;
+        }
+        for(int j = 0; j < nums.size(); ++j){
+            int prevNum = nums[checkSum[nums[j]]];
+            std::cout << prevNum << " " << nums[j] << " " << checkSum[nums[j]] << endl;
+            
+            if (checkSum.count(nums[j]) && prevNum + nums[j] == target && j != checkSum[nums[j]]){
+                result.push_back(checkSum[nums[j]]);
+                result.push_back(j);
+                return result;
+            }
+        }
+        
+        return result;
+    }
+
+};
